dc68bd9acfdf5fa063f22c8055cb9fdc0fa58dda
sunbojason <sunbojason@foxmail.com>
2021-11-09 16:03:03 +0100

    all modules work

---
+++ script_flight.py
@@ -0,0 +1,216 @@
+import logging
+import time
+from threading import Timer
+
+import cflib.crtp
+from cflib.crazyflie import Crazyflie
+from cflib.crazyflie.log import LogConfig
+from cflib.utils import uri_helper
+
+import ast
+import sys
+from NatNetClient import NatNetClient
+
+import numpy as np
+import math
+
+from tools.kine_UAV import Kine_UAV
+
+uri = uri_helper.uri_from_env(default='radio://0/80/2M/E7E7E7E7E7')
+
+# Only output errors from the logging framework
+logging.basicConfig(level=logging.ERROR)
+
+pos_ot = np.zeros((1,3))
+phi, theta, psi, vex, vey, vez = 0.0, 0.0, 0.0, 0.0, 0.0, 0.0
+
+kine_UAV = Kine_UAV()
+class LoggingExample:
+    """
+    Simple logging example class that logs the Stabilizer from a supplied
+    link uri and disconnects after 5s.
+    """
+
+    def __init__(self, link_uri):
+        """ Initialize and run the example with the specified link_uri """
+
+        self._cf = Crazyflie(rw_cache='./cache')
+
+        # Connect some callbacks from the Crazyflie API
+        self._cf.connected.add_callback(self._connected)
+        self._cf.disconnected.add_callback(self._disconnected)
+        self._cf.connection_failed.add_callback(self._connection_failed)
+        self._cf.connection_lost.add_callback(self._connection_lost)
+
+        print('Connecting to %s' % link_uri)
+
+        # Try to connect to the Crazyflie
+        self._cf.open_link(link_uri)
+
+        # Variable used to keep main loop occupied until disconnect
+        self.is_connected = True
+
+    def _connected(self, link_uri):
+        """ This callback is called form the Crazyflie API when a Crazyflie
+        has been connected and the TOCs have been downloaded."""
+        print('Connected to %s' % link_uri)
+
+        self._cf.param.set_value('kalman.resetEstimation', '1')
+        time.sleep(0.1)
+        self._cf.param.set_value('kalman.resetEstimation', '0')
+        time.sleep(2)
+
+        # The definition of the logconfig can be made before connecting
+        self._lg_stab = LogConfig(name='Stabilizer', period_in_ms=10)
+        self._lg_stab.add_variable('stabilizer.roll', 'float')
+        self._lg_stab.add_variable('stabilizer.pitch', 'float')
+        self._lg_stab.add_variable('stabilizer.yaw', 'float')
+        self._lg_stab.add_variable('stateEstimate.vx', 'float')
+        self._lg_stab.add_variable('stateEstimate.vy', 'float')
+        self._lg_stab.add_variable('stateEstimate.vz', 'float')
+        # The fetch-as argument can be set to FP16 to save space in the log packet
+        self._lg_stab.add_variable('pm.vbat', 'FP16')
+
+        # Adding the configuration cannot be done until a Crazyflie is
+        # connected, since we need to check that the variables we
+        # would like to log are in the TOC.
+        try:
+            self._cf.log.add_config(self._lg_stab)
+            # This callback will receive the data
+            self._lg_stab.data_received_cb.add_callback(self._stab_log_data)
+            # This callback will be called on errors
+            self._lg_stab.error_cb.add_callback(self._stab_log_error)
+            # Start the logging
+            self._lg_stab.start()
+        except KeyError as e:
+            print('Could not start log configuration,'
+                  '{} not found in TOC'.format(str(e)))
+        except AttributeError:
+            print('Could not add Stabilizer log config, bad configuration.')
+
+        # Start a timer to disconnect in 10s
+        t = Timer(300, self._cf.close_link) # adjust it !!!!
+        t.start()
+
+    def _stab_log_error(self, logconf, msg):
+        """Callback from the log API when an error occurs"""
+        print('Error when logging %s: %s' % (logconf.name, msg))
+
+    def _stab_log_data(self, timestamp, data, logconf):
+        """Callback from a the log API when data arrives"""
+        global phi, theta, psi, vex, vey, vez
+        phi = data['stabilizer.roll']
+        theta = -data['stabilizer.pitch'] # make it right hand
+        psi = data['stabilizer.yaw']
+        vbx = data['stateEstimate.vx']
+        vby = data['stateEstimate.vy']
+        vbz = data['stateEstimate.vz']
+
+        v_body = np.array([vbx,vby,vbz])
+        v_earth = np.matmul(kine_UAV.rotation_matrix(phi, theta), v_body)
+        vex = v_earth[0]
+        vey = v_earth[1]
+        vez = v_earth[2]
+        
+
+        pass
+
+    def _connection_failed(self, link_uri, msg):
+        """Callback when connection initial connection fails (i.e no Crazyflie
+        at the specified address)"""
+        print('Connection to %s failed: %s' % (link_uri, msg))
+        self.is_connected = False
+
+    def _connection_lost(self, link_uri, msg):
+        """Callback when disconnected after a connection has been made (i.e
+        Crazyflie moves out of range)"""
+        print('Connection to %s lost: %s' % (link_uri, msg))
+
+    def _disconnected(self, link_uri):
+        """Callback when the Crazyflie is disconnected (called in all cases)"""
+        print('Disconnected from %s' % link_uri)
+        self.is_connected = False
+
+
+
+# This is a callback function that gets connected to the NatNet client and called once per mocap frame.
+def receiveNewFrame( frameNumber, markerSetCount, unlabeledMarkersCount, rigidBodyCount, skeletonCount,
+                    labeledMarkerCount, latency, timecode, timecodeSub, timestamp, isRecording, trackedModelsChanged ):
+	pass
+
+
+# This is a callback function that gets connected to the NatNet client. It is called once per rigid body per frame
+def receiveRigidBodyFrame( id, position, rotation ):
+    global pos_ot
+    if id==1:
+        # print(position)
+        # print(rotation)
+        
+        # opti_track z x y
+        # crazyflie x y z
+        pos_ot[0,0] = position[2]
+        pos_ot[0,1] = position[0]
+        pos_ot[0,2] = position[1]
+    
+
+
+if __name__ == '__main__':
+
+    streamingClient = NatNetClient() # Create a new NatNet client
+    streamingClient.newFrameListener = receiveNewFrame
+    streamingClient.rigidBodyListener = receiveRigidBodyFrame
+    streamingClient.run() # Run perpetually on a separate thread.
+
+ # Initialize the low-level drivers
+    cflib.crtp.init_drivers()
+    le = LoggingExample(uri)
+
+    file = open('./dat00.csv', 'w')
+    file.write('timeStamp, OTx, OTy, OTz, vex, vey, vez, roll, pitch, yaw\n')
+
+    # The Crazyflie lib doesn't contain anything to keep the application alive,
+    # so this is where your application should do something. In our case we
+    # are just waiting until we are disconnected.
+    time.sleep(5)
+    time0 = round(time.time()*1000)%1000000
+    while le.is_connected:
+        try:
+            time.sleep(0.01)
+            time_now = round(time.time()*1000)%1000000-time0 # timestamp (ms)
+            file.write('{}, {}, {}, {}, {}, {}, {}, {}, {}, {}\n'.format(time_now, pos_ot[0,0], pos_ot[0,1], pos_ot[0,2], vex, vey, vez, phi, theta, psi))
+            # le._cf.commander.send_setpoint(roll, pitch, yaw, thrust) # thrust 0-FFFF 
+            print('vex',vex, 'theta', theta)
+            
+            # for y in range(10):
+            #     le._cf.commander.send_hover_setpoint(0, 0, 0, y / 25)
+            #     time.sleep(0.1)
+
+            # for _ in range(20):
+            #     le._cf.commander.send_hover_setpoint(0, 0, 0, 0.4)
+            #     time.sleep(0.1)
+
+            # for _ in range(50):
+            #     le._cf.commander.send_hover_setpoint(0.5, 0, 36 * 2, 0.4)
+            #     time.sleep(0.1)
+
+            # for _ in range(50):
+            #     le._cf.commander.send_hover_setpoint(0.5, 0, -36 * 2, 0.4)
+            #     time.sleep(0.1)
+
+            # for _ in range(20):
+            #     le._cf.commander.send_hover_setpoint(0, 0, 0, 0.4)
+            #     time.sleep(0.1)
+
+            # for y in range(10):
+            #     le._cf.commander.send_hover_setpoint(0, 0, 0, (10 - y) / 25)
+            #     time.sleep(0.1)
+
+            # le._cf.commander.send_stop_setpoint()
+
+        except KeyboardInterrupt:
+            print("stop")
+            ## landing procedure
+            # for i in range(50):
+            #     le[i]._cf.commander.send_hover_setpoint(0, 0, 0, 0.6-i*0.01)
+            #     time.sleep(0.1)
+            raise
---
+++ tools/kine_UAV.py
@@ -0,0 +1,251 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'''
+@File        :kine_UAV.py
+@Description :
+@Date        :2021/09/29 14:08:39
+@Author      :Bo Sun
+'''
+
+
+from matplotlib.pyplot import axis
+import numpy as np
+
+class Kine_UAV():
+    """
+    UAV kinematics, yaw is fixed
+    """
+    def __init__(self) -> None:
+        self.tau_phi = 0.5
+        self.tau_theta = 0.5
+        self.K_phi = 1
+        self.K_theta = 1
+        self.Ax = 0.1
+        self.Ay = 0.1
+        self.Az = 0.2
+        self.g = 9.81
+        self.T_trim = self.g
+
+    def rotation_matrix(self, phi, theta):
+        """
+        The rotation matrix is built based on https://arxiv.org/abs/2003.05853
+        Pitch - Roll
+        """
+        # Rx = np.array([[1, 0, 0], [0, np.cos(phi), np.sin(phi)], [0, np.sin(phi), np.cos(phi)]])
+        # Ry = np.array([[np.cos(theta), 0, np.sin(theta)], [0, 1, 0], [-np.sin(theta), 0, np.cos(theta)]])
+
+        # R = np.matmul(Rx,Ry)
+
+        R = np.array([[np.cos(theta), 0, np.sin(theta)], [np.sin(phi)*np.sin(theta), np.cos(phi), -np.sin(phi)*np.cos(theta)], 
+        [-np.cos(phi)*np.sin(theta), np.sin(phi), np.cos(phi)*np.cos(theta)]])
+
+        return R
+
+
+
+    def kine_nl_all(self, state, control):
+        """
+        nonlinear system
+        These Kinnmatics euqations are built based on https://doi.org/10.1109/LRA.2020.3010730
+        Yaw is fixed
+        """
+        p = state[0:3]
+        v = state[3:6]
+        phi = state[6]
+        theta = state[7]
+        T = control[0]
+        phi_ref = control[1]
+        theta_ref = control[2]
+
+        R = self.rotation_matrix(phi, theta)
+
+        d_p = v
+        d_v = np.matmul(R,np.array([0,0,T])) + np.array([0,0,-self.g]) - np.matmul(np.diag([self.Ax, self.Ay, self.Az]),v)
+        d_phi = 1/self.tau_phi*(self.K_phi*phi_ref-phi)
+        d_theta = 1/self.tau_theta*(self.K_theta*theta_ref - theta)
+
+        d_state = np.array([d_p,d_v])
+        d_state = np.append(d_state, d_phi)
+        d_state = np.append(d_state, d_theta)
+
+        return d_state
+
+    def sys_nl_state_space(self, phi, theta):
+        """
+        nonlinear system
+        states = [p,v,phi,theta]
+        """
+        A = np.array([
+            [0,0,0,1,0,0,0,0],
+            [0,0,0,0,1,0,0,0],
+            [0,0,0,0,0,1,0,0],
+            [0,0,0,-self.Ax,0,0,0,0],
+            [0,0,0,0,-self.Ay,0,0,0],
+            [0,0,0,0,0,-self.Az,0,0],
+            [0,0,0,0,0,0,-1/self.tau_phi,0],
+            [0,0,0,0,0,0,0,-1/self.tau_theta]
+        ])
+
+        B = np.array([
+            [0,0,0],
+            [0,0,0],
+            [0,0,0],
+            [np.sin(theta),0,0],
+            [-np.sin(phi)*np.cos(theta),0,0],
+            [np.cos(phi)*np.cos(theta),0,0],
+            [0,self.K_phi/self.tau_phi,0],
+            [0,0,self.K_theta/self.tau_theta]
+        ])
+        
+        dist_grav = np.array([0,0,0,0,0,-self.g,0,0])
+
+        return A, B, dist_grav
+
+    def kine_nl_simple(self, state, control):
+        """
+        These Kinnmatics euqations are built based on https://doi.org/10.1109/LRA.2020.3010730
+        Yaw is fixed
+        """
+       
+        A, B, dist_grav = self.sys_nl_state_space(state[6], state[7])
+
+        d_state = np.matmul(A,state) + np.matmul(B,control) + dist_grav
+
+        return d_state
+
+    def sys_linear_state_space(self):
+        """
+        linearized system
+        states = [p,v,phi,theta]
+        trimed condition 
+        """
+
+        A = np.array([
+            [0,0,0,1,0,0,0,0],
+            [0,0,0,0,1,0,0,0],
+            [0,0,0,0,0,1,0,0],
+            [0,0,0,-self.Ax,0,0,0,self.T_trim],
+            [0,0,0,0,-self.Ay,0,-self.T_trim,0],
+            [0,0,0,0,0,-self.Az,0,0],
+            [0,0,0,0,0,0,-1/self.tau_phi,0],
+            [0,0,0,0,0,0,0,-1/self.tau_theta]
+        ])
+
+        B = np.array([
+            [0,0,0],
+            [0,0,0],
+            [0,0,0],
+            [0,0,0],
+            [0,0,0],
+            [1,0,0],
+            [0,self.K_phi/self.tau_phi,0],
+            [0,0,self.K_theta/self.tau_theta]
+        ])
+
+        return A, B
+
+    def augsys_linear(self):
+        A, B = self.sys_linear_state_space()
+        C = np.array([
+            [1,0,0,0,0,0,0,0],
+            [0,1,0,0,0,0,0,0],
+            [0,0,1,0,0,0,0,0]  
+        ])
+
+        a_temp = np.concatenate((A,np.zeros((8,3))), axis = 1)
+        b_temp = np.concatenate((C,np.zeros((3,3))), axis = 1)
+        A_aug = np.concatenate((a_temp,b_temp), axis = 0)
+
+        B_aug = np.concatenate((B,np.zeros((3,3))), axis = 0)
+        
+
+
+        return A_aug, B_aug
+
+
+    # def augsys_state_space(self, phi, theta):
+    #     """
+    #     states= [p-ref,v, phi, theta, ref]
+    #     The dynamics of ref does not matter
+    #     It is assumed constant here between two steps
+    #     """
+    #     A_aug = np.array([
+    #         [0,0,0,1,0,0,0,0,0,0,0],
+    #         [0,0,0,0,1,0,0,0,0,0,0],
+    #         [0,0,0,0,0,1,0,0,0,0,0],
+    #         [0,0,0,-self.Ax,0,0,0,0,0,0,0],
+    #         [0,0,0,0,-self.Ay,0,0,0,0,0,0],
+    #         [0,0,0,0,0,-self.Az,0,0,0,0,0],
+    #         [0,0,0,0,0,0,-1/self.tau_phi,0,0,0,0],
+    #         [0,0,0,0,0,0,0,-1/self.tau_theta,0,0,0],
+    #         [1,0,0,0,0,0,0,0,0,0,0],
+    #         [0,1,0,0,0,0,0,0,0,0,0],
+    #         [0,0,1,0,0,0,0,0,0,0,0]
+    #     ])
+
+    #     B_aug = np.array([
+    #         [0,0,0],
+    #         [0,0,0],
+    #         [0,0,0],
+    #         [np.sin(theta),0,0],
+    #         [-np.sin(phi)*np.cos(theta),0,0],
+    #         [np.cos(phi)*np.cos(theta),0,0],
+    #         [0,self.K_phi/self.tau_phi,0],
+    #         [0,0,self.K_theta/self.tau_theta],
+    #         [0,0,0],
+    #         [0,0,0],
+    #         [0,0,0]
+    #     ])    
+            
+    #     dist_grav_aug = np.array([0,0,0,0,0,-self.g,0,0,0,0,0])
+
+    #     return A_aug, B_aug, dist_grav_aug
+
+    # def augsys_kine(self, state, control, ref):
+
+    #     state_aug = np.concatenate(state[0:3]-ref, state[3:], ref)
+
+    #     A_aug, B_aug, dist_grav_aug = self.augsys_state_space(state[6], state[7])
+        
+    #     d_state_aug = np.matmul(A_aug, state_aug) + np.matmul(B_aug,control) + dist_grav_aug
+
+    #     return d_state_aug
+        
+
+
+
+
+class Ref_pos():
+    def __init__(self) -> None:
+        pass
+    
+    def circle(self, time):
+        """
+        A circle refernce path
+        Li, Shushuai, Yaonan Wang, and Jianhao Tan. 
+        "Adaptive and robust control of quadrotor aircrafts with input saturation." 
+        Nonlinear Dynamics 89.1 (2017): 255-265.
+        https://doi.org/10.1007/s11071-017-3451-z
+        """
+        x_r = 0.5*np.cos(np.pi*time/20)
+        y_r = 0.5*np.sin(np.pi*time/20)
+        z_r = 3 - 2*np.cos(np.pi*time/20)
+
+        return np.array([x_r,y_r,z_r])
+        
+    def eight(self, time):
+        x_r = 0.5*np.cos(np.pi*time/20)
+        y_r = 1*np.sin(np.pi*time/10)
+        z_r = 3 - 2*np.cos(np.pi*time/20)
+
+        return np.array([x_r,y_r,z_r])
+
+
+
+
+
+
+
+
+        
